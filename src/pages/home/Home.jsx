870919fabe54516f0937cafd1e2fd62b78a39ede
Harshal Tupe <harshaltupe12@gmail.com>
2023-12-06 19:35:06 +0530

    Creating Home layout

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./home.scss"
+import Sidebar from '../../components/sidebar/Sidebar'
 
 const Home = () => {
   return (
     <div className='home'>
-        <h1 className='title'> Home</h1>
+        <Sidebar/>
+        <div className="homeContainer">Container form home</div>
     </div>
   )
 }
