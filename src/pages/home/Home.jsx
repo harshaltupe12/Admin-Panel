e095c511f4e8967d0c1b1f20546b6fccc1f1fcae
Harshal Tupe <harshaltupe12@gmail.com>
2023-12-07 16:56:22 +0530

    added components to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import "./home.scss"
 import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
 
 const Home = () => {
   return (
     <div className='home'>
         <Sidebar/>
-        <div className="homeContainer">Container form home</div>
+        <div className="homeContainer">
+          <Navbar/> 
+          Home Stuff here...
+        </div>
     </div>
   )
 }
