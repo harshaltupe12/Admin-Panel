ee9881b671a423423fdacce2db690026885ed907
Harshal Tupe <harshaltupe12@gmail.com>
2023-12-07 16:54:43 +0530

    nav bar created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import "./navbar.scss"
-
+import SearchIcon from '@mui/icons-material/Search';
+import TranslateOutlinedIcon from '@mui/icons-material/TranslateOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import PinchOutlinedIcon from '@mui/icons-material/PinchOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder='Search..' />
+          <SearchIcon/>
+        </div>
+        <div className="items">
+          <div className="item">
+            <TranslateOutlinedIcon/> 
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon/> 
+          </div>
+          <div className="item">
+            <PinchOutlinedIcon/> 
+          </div>
+          <div className="item">
+            <NotificationsOutlinedIcon/>      
+          </div>
+          <div className="item">
+            <FormatListBulletedOutlinedIcon/> 
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
