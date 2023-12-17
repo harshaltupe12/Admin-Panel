19dee64e17aee6c1bc76554a664a8d39559bc6c1
Harshal Tupe <harshaltupe12@gmail.com>
2023-12-17 19:21:32 +0530

    Render Widget component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./home.scss"
 import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar'
+import Widget from '../../components/widget/Widget'
 
 const Home = () => {
   return (
@@ -9,7 +10,13 @@ const Home = () => {
         <Sidebar/>
         <div className="homeContainer">
           <Navbar/> 
-          Home Stuff here...
+          <div className="widget">
+            <Widget type="user"/>
+            <Widget type="order"/>
+            <Widget type="earning"/>
+            <Widget type="balance"/>
+          </div>
+          <div className="charts"></div>
         </div>
     </div>
   )
