d8d5727f9ece2e6446aaa4e8ed703c84597bda1d
Harshal Tupe <harshaltupe12@gmail.com>
2023-12-18 15:29:00 +0530

    Rendering Featured & Chart Components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,9 @@ import "./home.scss"
 import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar'
 import Widget from '../../components/widget/Widget'
+import Featured from '../../components/featured/Featured'
+import Chart from '../../components/chart/Chart'
+
 
 const Home = () => {
   return (
@@ -16,7 +19,10 @@ const Home = () => {
             <Widget type="earning"/>
             <Widget type="balance"/>
           </div>
-          <div className="charts"></div>
+          <div className="charts">
+            <Featured/>
+            <Chart/>
+          </div>
         </div>
     </div>
   )
