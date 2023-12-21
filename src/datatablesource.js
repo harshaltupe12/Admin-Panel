c8573f30380a139930fd7d591fd096ded3e55382
Harshal Tupe <harshaltupe12@gmail.com>
2023-12-21 21:31:41 +0530

    Create a js file for for storing static data

---
+++ src/datatablesource.js
@@ -0,0 +1,103 @@
+//temporary data
+
+export const userColumn = [
+    { field: 'id', headerName: 'ID', width: 100 },
+    { field: 'user', headerName: 'User', width: 250, renderCell: (params)=>{
+        return(
+            <div className="cellWithImg">
+                <img src={ params.row.img } alt=" " className="cellImg"/>
+                { params.row.username }
+            </div>
+        )
+    }},
+    { field: "email", headerName:"Email", width: 250},
+    { field: "age", headerName:"Age", width: 130},
+    { field: "status", headerName:"Status", width: 180, renderCell:(params)=>{
+        return(
+            <div className={`cellWithStatus ${params.row.status}`}>{params.row.status}</div>
+        )
+    }},
+];
+
+export const userRows = [
+    {
+      id: 1,
+      username: "Snow",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      status: "active",
+      email: "snow1@gmail.com",
+      age: 35,
+    },
+    {
+      id: 2,
+      username: "Jamie Lannister",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      email: "flex2@gmail.com",
+      status: "passive",
+      age: 42,
+    },
+    {
+      id: 3,
+      username: "Lannister",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      email: "grand@gmail.com",
+      status: "pending",
+      age: 45,
+    },
+    {
+      id: 4,
+      username: "Stark",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      email: "harshal@gmail.com",
+      status: "active",
+      age: 16,
+    },
+    {
+      id: 5,
+      username: "Targaryen",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      email: "jay@gmail.com",
+      status: "passive",
+      age: 22,
+    },
+    {
+      id: 6,
+      username: "Melisandre",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      email: "kaushik@gmail.com",
+      status: "active",
+      age: 15,
+    },
+    {
+      id: 7,
+      username: "Clifford",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      email: "mayur@gmail.com",
+      status: "passive",
+      age: 44,
+    },
+    {
+      id: 8,
+      username: "Frances",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      email: "arya@gmail.com",
+      status: "active",
+      age: 36,
+    },
+    {
+      id: 9,
+      username: "Roxie",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      email: "tommy@gmail.com",
+      status: "pending",
+      age: 65,
+    },
+    {
+      id: 10,
+      username: "Roxie",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      email: "gta@gmail.com",
+      status: "active",
+      age: 65,
+    },
+  ];
