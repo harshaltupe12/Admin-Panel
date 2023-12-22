9e36a4ae9eff073442baefce0902a7690395799c
Harshal Tupe <harshaltupe12@gmail.com>
2023-12-22 16:18:19 +0530

    Context API used

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import Login from "./pages/login/Login.jsx";
 import Single from "./pages/single/Single.jsx";
 import New from "./pages/new/New.jsx";
 import {BrowserRouter, Routes, Route}from "react-router-dom";
+import { userInputs, productInputs } from "./formSource.js";
+import "./style/dark.scss"
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext.js";
 
 function App() {
+  const {darkMode} = useContext(DarkModeContext)
   return (
-    <div className="App">
+    <div className={darkMode ? "App dark" : "App"}>
       <BrowserRouter>
       <Routes>
         <Route path="/" >
@@ -16,12 +21,12 @@ function App() {
           <Route path="users">
             <Route index element={<List/>}/>
             <Route path=":userId" element={<Single/>}/>
-            <Route path="new" element={<New/>}/>
+            <Route path="new" element={<New inputs={userInputs} title = "Add new user"/>}/>
           </Route>
           <Route path="products">
             <Route index element={<List/>}/>
             <Route path=":productId" element={<Single/>}/>
-            <Route path="new" element={<New/>}/>
+            <Route path="new" element={<New inputs={productInputs} title = "Add new Product"/>}/>
           </Route>
         </Route>
       </Routes>
