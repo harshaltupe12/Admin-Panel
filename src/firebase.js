4869570cda447521dc1b81dd25369a81c046b9cd
Harshal Tupe <harshaltupe12@gmail.com>
2023-12-28 14:13:46 +0530

    Add firebase for Database Conectivity

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAlSLygfu32VWUaSKUZe1XwZUrsl8CJ4dE",
+  authDomain: "dashboard-2a9c6.firebaseapp.com",
+  projectId: "dashboard-2a9c6",
+  storageBucket: "dashboard-2a9c6.appspot.com",
+  messagingSenderId: "981923185913",
+  appId: "1:981923185913:web:8f63f71b5b7fdbde8c8e28"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
